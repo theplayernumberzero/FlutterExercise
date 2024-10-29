e797eb8f2769cfd6557404bfa022740161263b0b
theplayernumberzero <bxkuygulamalar.swe@gmail.com>
2024-10-29 22:35:12 +0300

    Flutter Exercise: Coklu Ekran Destegi -2

---
+++ coklu_ekran_destegi2/lib/main.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+
+  @override
+  Widget build(BuildContext context) {
+
+    var ekranBilgisi = MediaQuery.of(context);
+    final double ekranYuksekligi = ekranBilgisi.size.height;
+    final double ekranGenisligi = ekranBilgisi.size.width;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: EdgeInsets.only(top:ekranYuksekligi/100),
+            child: SizedBox(
+                width: ekranGenisligi/2,
+                height: ekranYuksekligi/5,
+                child: Image.asset("resimler/resim.jpg")
+            ),
+          ),
+          Container(width: ekranGenisligi,height: ekranYuksekligi/4,color: Colors.red,),
+          Text("Merhaba",style: TextStyle(fontSize: ekranGenisligi/10),),
+        ],
+      ),
+
+    );
+  }
+}
